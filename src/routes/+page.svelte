4928dc0ca4cf025f18ef2e1b1e46fb90f000be5f
Ethan <elephant9000@gmail.com>
2023-08-24 16:53:22 -0400

    adds schedule

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-white pt-24 pb-10 sm:pt-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-3xl lg:mx-0">
+      <p class="text-base font-semibold leading-7 text-indigo-600">
+        Get the help you need
+      </p>
+      <h2
+        class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+      >
+        Schedule Time with Ethan
+      </h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        Schedule a time to talk with Ethan about your project, assignment, or
+        just to chat. Be sure to use your Stark State email when requesting an
+        appointment.
+      </p>
+    </div>
+  </div>
+</div>
+
+<div>
+  <!-- Calendly inline widget begin -->
+  <div
+    class="calendly-inline-widget"
+    data-url="https://calendly.com/ethan-hancock/general-questions?hide_event_type_details=1&hide_gdpr_banner=1"
+    style="min-width:320px; height:700px;"
+  />
+  <script
+    type="text/javascript"
+    src="https://assets.calendly.com/assets/external/widget.js"
+    async
+  ></script>
+  <!-- Calendly inline widget end -->
+</div>
